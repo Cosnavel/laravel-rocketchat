45778ef433669cb92a8ebe393366014f4c6bfcd5
Cosnavel <coding@niclaskahlmeier.de>
2021-01-27 13:24:35 +0100

    [WIP] added Role Management

---
+++ src/Service/Role.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cosnavel\RocketChat\Service;
+
+trait Role
+{
+    public function getRoles()
+    {
+        $response = $this->buildUrl('roles.list', 'get');
+
+        return $this->response($response, json_decode($response->body())->roles);
+    }
+
+
+    public function addUserToRole(string $role, string $username)
+    {
+        $response = $this->buildUrl('roles.addUserToRole', 'post', ['role' => $role, 'username' => $username]);
+
+        return $this->response($response, json_decode($response->body())->success);
+    }
+
+
+    public function getUsersInRole(string $role)
+    {
+        $response = $this->buildUrl('roles.getUsersInRole', 'get', ['role' => $role]);
+
+        return $this->response($response, json_decode($response->body())->users);
+    }
+}
